16af2de514d779c04cd3bf93a456c223218b20c4
Vincenzo Montana s322977 <s322977@studenti.polito.it>
2025-03-31 11:23:32 +0200

    classmix update

--- PIDNet-main/tools/classmix.py
+++ PIDNet-main/tools/classmix.py
@@ -16,15 +16,6 @@ import numpy as np
 import cv2
 
 
-def unnormalize(img, mean, std):
-    """
-    img: Tensor with shape (C, H, W)
-    mean, std: lists of means and stds for each channel
-    """
-    for t, m, s in zip(img, mean, std):
-        t.mul_(s).add_(m)
-    return img
-
 def show_mixed_visualization(x_s, y_s, x_t, y_t, x_mixed, y_mixed, bd_mixed):
     # Move tensors to CPU and convert to NumPy
     x_s = x_s[0].cpu().clone()
@@ -85,21 +76,22 @@ def show_mixed_visualization(x_s, y_s, x_t, y_t, x_mixed, y_mixed, bd_mixed):
     axs[1, 3].axis("off")  # Can be used for something else if needed
 
     plt.tight_layout()
-    plt.show()
+    plt.show() 
 
-
-def classmix(x1, y1, x2, y2):
+def classmix(x1, y1, x2, y2, verbose=False):
     classes = y1.unique().tolist()
     selected = random.sample(classes, len(classes) // 2)
     mask = torch.zeros_like(y1, dtype=torch.bool)
+    if verbose:
+        print(f"Classes from the source domain: {selected}") 
     for c in selected:
-        mask |= (y1 == c)
+        mask |= (y1 == c) # c
     mask = mask.unsqueeze(1)
     x_mix = torch.where(mask, x1, x2)
     y_mix = torch.where(mask.squeeze(1), y1, y2)
     return x_mix, y_mix, mask.squeeze(1)
 
-def generate_safe_edge_map(y_mixed, source_mask, conf_t, threshold, edge_size=4, edge_pad=False, ignore_label=255):
+def generate_safe_edge_map(y_mixed, source_mask, edge_size=4, edge_pad=False, ignore_label=255):
     B, H, W = y_mixed.shape
     edge_maps = []
     kernel = np.ones((edge_size, edge_size), np.uint8)
@@ -109,18 +101,13 @@ def generate_safe_edge_map(y_mixed, source_mask, conf_t, threshold, edge_size=4,
     for i in range(B):
         label_np = y_mixed[i].cpu().numpy().astype(np.uint8)
         mask_np = source_mask[i].cpu().numpy().astype(np.uint8)
-        conf_np = conf_t[i].cpu().numpy()
-
-        confident_regions = conf_np > threshold      # matrix of 0 and 1
-        label_masked = label_np * mask_np * confident_regions   #confident_regions filters out uncertain edges
-
+        label_masked = label_np * mask_np
         edge = cv2.Canny(label_masked, 0.1, 0.2)
         if edge_pad:
             edge = edge[y_k_size:-y_k_size, x_k_size:-x_k_size]
             edge = np.pad(edge, ((y_k_size, y_k_size), (x_k_size, x_k_size)), mode='constant')
         edge = (cv2.dilate(edge, kernel, iterations=1) > 50).astype(np.uint8)
         edge[mask_np == 0] = ignore_label
-        
         edge_maps.append(torch.tensor(edge, dtype=torch.long).unsqueeze(0))
 
     return torch.cat(edge_maps, dim=0).to(y_mixed.device)
@@ -144,10 +131,16 @@ if __name__ == '__main__':
     imgnet = 'imagenet' in config.MODEL.PRETRAINED
 
     train_dataset = eval('datasets.'+config.DATASET.DATASET)(
-        root=config.DATASET.ROOT, list_path=config.DATASET.TRAIN_SET,
-        num_classes=config.DATASET.NUM_CLASSES, multi_scale=config.TRAIN.MULTI_SCALE,
-        flip=config.TRAIN.FLIP, enable_augmentation=True, ignore_label=config.TRAIN.IGNORE_LABEL,
-        base_size=config.TRAIN.BASE_SIZE, crop_size=crop_size, scale_factor=config.TRAIN.SCALE_FACTOR,
+        root=config.DATASET.ROOT, 
+        list_path=config.DATASET.TRAIN_SET,
+        num_classes=config.DATASET.NUM_CLASSES, 
+        multi_scale=config.TRAIN.MULTI_SCALE,
+        flip=config.TRAIN.FLIP, 
+        enable_augmentation=True, 
+        ignore_label=config.TRAIN.IGNORE_LABEL,
+        base_size=config.TRAIN.BASE_SIZE, 
+        crop_size=crop_size, 
+        scale_factor=config.TRAIN.SCALE_FACTOR,
         horizontal_flip=config.TRAIN.AUGMENTATION.TECHNIQUES.HORIZONTAL_FLIP,
         gaussian_blur=config.TRAIN.AUGMENTATION.TECHNIQUES.GAUSSIAN_BLUR,
         multiply=config.TRAIN.AUGMENTATION.TECHNIQUES.MULTIPLY,
@@ -178,8 +171,12 @@ if __name__ == '__main__':
     model = nn.DataParallel(model, device_ids=gpus).cuda()
 
     trainloader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=batch_size, shuffle=config.TRAIN.SHUFFLE,
-        num_workers=config.WORKERS, pin_memory=False, drop_last=True)
+        train_dataset, 
+        batch_size=batch_size, 
+        shuffle=config.TRAIN.SHUFFLE,
+        num_workers=config.WORKERS, 
+        pin_memory=False, 
+        drop_last=True)
 
     targetloader = torch.utils.data.DataLoader(
         target_dataset, batch_size=batch_size, shuffle=config.TRAIN.SHUFFLE,
@@ -187,15 +184,23 @@ if __name__ == '__main__':
 
     source_iter = iter(trainloader)
     target_iter = iter(targetloader)
-
+    n_to_show = 1
     for batch_idx, (source_batch, target_batch) in enumerate(zip(source_iter, target_iter)):
-        if batch_idx > 0:
-            break
+        if batch_idx > n_to_show-1:
+           break
 
         x_s, y_s, *extra = source_batch
-        x_t, _, *extra = target_batch
+        x_t, real_gt, *extra = target_batch
         x_s, y_s = x_s.cuda(), y_s.cuda()
         x_t = x_t.cuda()
+        print("Label map:\nbackground – 1\nbuilding – 2\nroad – 3\nwater – 4\nbarren – 5\nforest – 6\nagriculture – 7\nno-data regions-0 (should be ignored)")
+
+        print(f"Unique labels found in urban image: {y_s.unique().tolist()}")
+        print(f"Unique labels found in rural image: {real_gt.unique().tolist()}")
+        
+        if 7 in real_gt.unique().tolist() or 7 in y_s.unique().tolist():
+            print("Found")
+            break
 
         with torch.no_grad():
             logits_t = model.module.model(x_t)[-2]
@@ -203,10 +208,20 @@ if __name__ == '__main__':
                 logits_t, size=x_t.shape[2:], mode='bilinear', align_corners=config.MODEL.ALIGN_CORNERS
             )
             pseudo_t = torch.argmax(logits_t, dim=1)
-            conf_t = torch.softmax(logits_t, dim=1).max(dim=1)[0]
+            conf_t = torch.softmax(logits_t, dim=1).max(dim=1)[0] # It is a [B, H, W] tensor representing the maximum class probability per pixel
+            
+            
+            use_confidence = False
+            if use_confidence:
+                confidence_mask = conf_t > config.TRAIN.DACS.THRESHOLD # It tells the position of the pixel whose pseudo-labels have a confidence higher than  the threshold
+                pseudo_t = torch.where(
+                    confidence_mask,
+                    pseudo_t,  # keep if confident
+                    torch.full_like(pseudo_t, config.TRAIN.IGNORE_LABEL) # ignore if not confident
+                )
 
-        x_mixed, y_mixed, source_mask = classmix(x_s, y_s, x_t, pseudo_t)  
-        bd_mixed = generate_safe_edge_map(y_mixed, source_mask, conf_t, threshold = 0.7, edge_size=3,
+        x_mixed, y_mixed, source_mask = classmix(x_s, y_s, x_t, pseudo_t, verbose=True)  
+        bd_mixed = generate_safe_edge_map(y_mixed, source_mask, edge_size=3,
                                           edge_pad=True, ignore_label=config.TRAIN.IGNORE_LABEL)        # secondo me threshold tra 0.5 e 0.7 massimo
 
         show_mixed_visualization(x_s, y_s, x_t, pseudo_t, x_mixed, y_mixed, bd_mixed)

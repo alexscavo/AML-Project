cc96375714e5c4289b39c813fa5b84594d76b02f
MattiaVi <s328941@studenti.polito.it>
2025-04-03 18:46:42 +0200

    labels num ?

--- PIDNet-main/utils/function.py
+++ PIDNet-main/utils/function.py
@@ -309,7 +309,7 @@ def train_adv(config, epoch, num_epoch, epoch_iters, base_lr,
         print(f"Labels dtype: {labels.dtype}, shape: {labels.shape}, unique values: {torch.unique(labels)}")
         assert labels.dtype == torch.long, "Labels devono essere di tipo torch.LongTensor"
         assert labels.min() >= 0, f"Labels contengono valori negativi: {labels.min()}"
-        assert labels.max() < 7, f"Labels contengono valori >= n_classes: {labels.max()}"
+        assert labels.max() < 8, f"Labels contengono valori >= n_classes: {labels.max()}"
 
         # 1. Forward seg net per dominio sorgente (supervisionato)
         losses, output_source, acc, loss_list = model(images_source, labels, bd_gts)  
